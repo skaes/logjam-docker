a110e35fc80a21a131a12805585589e1d7f9486d
Stefan Kaes <stefan.kaes@xing.com>
2015-05-02 14:25:48 +0200

    remove 160MB from passenger gem before building demo

---
+++ images/passenger/minify-passenger-apache2-module.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /opt/logjam/lib/ruby/gems/2.2.0/gems/passenger-4.0.59
+rm -rf download_cache debian.template doc dev ext boost resources packaging man test
+find . -name '*.[hc]' -o -name '*.[hc]pp' | xargs rm
+find . -name '*.[oa]' -o -name '*.l[oa]' -o -name '*.lai'  | xargs rm
+find . -name '*.Plo'  -o -name '*.log' | xargs rm
+find . -name configure -o -name config.status -o -name Makefile | xargs rm
