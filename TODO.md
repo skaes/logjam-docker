62fad6a7b4e30bf8f9627792e192f5c70b356a7a
Stefan Kaes <stefan.kaes@xing.com>
2015-05-03 20:39:21 +0200

    now using the c-importer

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-* use c-importer
 * explain how to add streams (or ad a UI to logjam?)
 * how to deal with web:enable/disable
 * using containers for development
--- images/tools/install-logjam-tools.sh
+++ images/tools/install-logjam-tools.sh
@@ -4,6 +4,6 @@ set -e
 git clone https://github.com/skaes/logjam-tools.git
 cd logjam-tools
 ./bin/install-libs --prefix /opt/logjam
-sh autogen.sh --prefix=/opt/logjam
+sh autogen.sh --prefix=/opt/logjam --with-opt-dir=/opt/logjam
 make
 make install
