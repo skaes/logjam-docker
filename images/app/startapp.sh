1322c4bda106a77beacd0df424ee6396118c557f
Stefan Kaes <stefan.kaes@xing.com>
2015-05-04 18:37:47 +0200

    can only generate daemons after environment variables have been set

--- images/app/startapp.sh
+++ images/app/startapp.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
+
+echo "preparing daemons"
+# this needs to be here as the actual name of the mongo server
+# is not known when the container is being built
+(cd /opt/logjam/app && bundle exec rake logjam:daemons:install && chown -R logjam.logjam service)
+
+# now we're goog to go
 echo "starting services"
 exec runsvdir -P /etc/service .....................................................
