a44411cd173483b2b121be00863b94e1aad28dfb
Stefan Kaes <stefan.kaes@xing.com>
2015-05-05 19:48:44 +0200

    faster startup for logjam app

--- images/app/startapp.sh
+++ images/app/startapp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "preparing daemons"
-# this needs to be here as the actual name of the mongo server
-# is not known when the container is being built
-(cd /opt/logjam/app && bundle exec rake logjam:daemons:install && chown -R logjam.logjam service)
+# insert actual database hostname into importer config
+DB_EXPR="(ENV['LOGJAM_PRODUCTION_DB'] || ENV['LOGJAMDB_NAME'] || 'localhost').split('/').last"
+DB_NAME=`ruby -e "puts $DB_EXPR"`
+sed -i -e "s/DB_PLACE_HOLDER/${DB_NAME}/" /opt/logjam/app/service/importer/importer.conf
 
-# now we're goog to go
+# now we're good to go
 echo "starting services"
 exec runsvdir -P /etc/service .....................................................
