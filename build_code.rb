b5d7bb5224516396078d3a107e87ed46db397a64
Stefan Kaes <stefan.kaes@xing.com>
2017-05-07 09:18:57 +0200

    updated code and tools package

--- build_code.rb
+++ build_code.rb
@@ -1,6 +1,6 @@
 name "logjam-code"
 version "0.5"
-iteration "4"
+iteration "5"
 
 vendor "skaes@railsexpress.de"
 
--- build_tools.rb
+++ build_tools.rb
@@ -3,7 +3,7 @@ suffix = ENV['LOGJAM_SUFFIX']
 
 name "logjam-tools#{suffix}"
 version "0.5"
-iteration "2"
+iteration "3"
 
 vendor "skaes@railsexpress.de"
 
