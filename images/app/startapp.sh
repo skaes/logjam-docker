7f196c08a08d1f22b74618dc1db5b3e205a55e99
Stefan Kaes <stefan.kaes@xing.com>
2019-10-20 07:53:13 +0200

    logjam-app container: remove leftover pid files on startup

--- images/app/startapp.sh
+++ images/app/startapp.sh
@@ -4,6 +4,9 @@ set -e
 # install daemons
 /docker/install-daemons.sh
 
+# remove leftover passenger pid files (unclean shutdown)
+rm -f /opt/logjam/app/passenger.*.pid /opt/logjam/app/passenger.*.lock
+
 # now we're good to go
 echo "starting services"
 exec runsvdir -P /etc/service .....................................................
