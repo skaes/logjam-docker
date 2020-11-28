2c8f84acd8479432ba25a4ffc8b076476a62c7f5
Stefan Kaes <stefan.kaes@xing.com>
2020-11-28 12:33:18 +0100

    bumped version to 0.9-11

--- build_app.rb
+++ build_app.rb
@@ -1,13 +1,13 @@
 name "logjam-app"
 version "0.8"
-iteration "10"
+iteration "11"
 
 vendor "skaes@railsexpress.de"
 
 depends "logjam-tools", ">= 0.9-5"
 depends "logjam-ruby", ">= 2.7.2"
 depends "logjam-passenger", ">= 0.6-16"
-depends "logjam-code", ">= 0.9-10"
+depends "logjam-code", ">= 0.9-11"
 depends "logrotate"
 depends "runit"
 depends "adduser"
