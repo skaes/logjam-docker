c06af421e218a4246b3a1c93cb08a3cb978e95c9
Stefan Kaes <stefan.kaes@xing.com>
2021-03-10 11:29:18 +0100

    bumped version to 0.11-4

--- build_app.rb
+++ build_app.rb
@@ -1,6 +1,6 @@
 name "logjam-app"
 version "0.11"
-iteration "3"
+iteration "4"
 
 vendor "skaes@railsexpress.de"
 
--- build_code.rb
+++ build_code.rb
@@ -1,6 +1,6 @@
 name "logjam-code"
 version "0.11"
-iteration "3"
+iteration "4"
 
 vendor "skaes@railsexpress.de"
 
