ab1e062c02ed354633b45a8f9a58020433290f4c
Stefan Kaes <stefan.kaes@xing.com>
2022-12-01 16:34:14 +0100

    require logjam-tools >= 0.16.4

--- build_app.rb
+++ build_app.rb
@@ -2,7 +2,7 @@ name "logjam-app"
 version YAML.load_file(Pathname.new(__dir__)+"versions.yml")["appl"]
 vendor "skaes@railsexpress.de"
 
-depends "logjam-tools", ">= 0.16.3"
+depends "logjam-tools", ">= 0.16.4"
 depends "logjam-passenger", ">= 0.15-1"
 depends "logjam-code", "= #{version}"
 depends "logrotate"
