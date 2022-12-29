aa2d966055711ed39a907edf80010f6f979e7ef5
Stefan Kaes <stefan.kaes@xing.com>
2022-12-29 20:23:44 +0100

    require new logjam-passenger version

--- build_app.rb
+++ build_app.rb
@@ -3,7 +3,7 @@ version YAML.load_file(Pathname.new(__dir__)+"versions.yml")["appl"]
 vendor "skaes@railsexpress.de"
 
 depends "logjam-tools", ">= 0.17.0"
-depends "logjam-passenger", ">= 0.16-1"
+depends "logjam-passenger", ">= 0.16-2"
 depends "logjam-code", "= #{version}"
 depends "logrotate"
 depends "runit"
