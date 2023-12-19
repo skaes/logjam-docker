e9a962c67b2d649cc61d74cb5d2f992838662a0d
Stefan Kaes <stefan.kaes@xing.com>
2023-12-19 09:15:27 +0100

    require passenger 6.0.19

--- build_app.rb
+++ build_app.rb
@@ -3,7 +3,7 @@ version YAML.load_file(Pathname.new(__dir__)+"versions.yml")["appl"]
 vendor "skaes@railsexpress.de"
 
 depends "logjam-tools", ">= 0.18.4"
-depends "logjam-passenger", ">= 0.17-1"
+depends "logjam-passenger", ">= 0.19-1"
 depends "logjam-code", "= #{version}"
 depends "logrotate"
 depends "runit"
