d106e9208816ab57f86889170ca365a5bb8ce694
Stefan Kaes <stkaes@gmail.com>
2024-10-10 15:59:10 +0200

    bump build dependencies

--- build_app.rb
+++ build_app.rb
@@ -2,7 +2,7 @@ name "logjam-app"
 version YAML.load_file(Pathname.new(__dir__)+"versions.yml")["appl"]
 vendor "skaes@railsexpress.de"
 
-depends "logjam-tools", ">= 0.19.0"
+depends "logjam-tools", ">= 0.20.2"
 depends "logjam-passenger", ">= 0.19-2"
 depends "logjam-code", "= #{version}"
 depends "logrotate"
